6e33fa11d8c23b8ddc8ae27c072540b8ae630fd8
Phillip N Tran <xizanagi@pc56.cs.ucdavis.edu>
2018-06-03 15:02:17 -0700

    Added Primary Keys to Tables Except for Meetings (help)

--- loadfakeu.py
+++ loadfakeu.py
@@ -6,10 +6,6 @@ import csv
 
 #######################################################################
 #   Stuff we still need to do:
-#   #FIXME: maybe change time into the actual time datatype
-#   #FIXME: Fix parsing so that it actually scans the integers and as integers instead of everything as strings
-#   #TODO:  Test this shit on larger datasets to see how fast it runs
-#   #FIXME: insert multiple tuples at a time rather than one at a time
 #   #TODO: restructure code to remove repetition
 ########################################################################
 
@@ -22,19 +18,20 @@ def clean_up(tuple):
 def create_tables(cur):
     ''' initializes the tables. I think every table should have the CID since the CID seems to connect our entities together. We can change l8er though'''
 
-    #FIXME: actually figure out how large everything is lol.
     cur.execute("CREATE TABLE course_tbl (\
-                CID Integer,\
+                CID Integer NOT NULL,\
                 TERM Integer,\
                 SUBJ CHAR(4),\
                 CRSE Integer,\
                 SEC Integer,\
-                UNITS VARCHAR(20)\
+                UNITS VARCHAR(20),\
+                PRIMARY KEY (CID, TERM,CRSE,SEC)\
     );")
 
-    #FIXME: maybe change time into the actual time datatype
+    #FIXME: What is our primary key here?
     cur.execute("CREATE TABLE meetings_tbl (\
                 CID Integer,\
+                TERM Integer,\
                 INSTRUCTORS VARCHAR(50),\
                 TYPE VARCHAR(30),\
                 DAYS VARCHAR(30),\
@@ -45,6 +42,7 @@ def create_tables(cur):
 
     cur.execute("CREATE TABLE seating_tbl (\
                 CID Integer,\
+                TERM Integer,\
                 SEAT VARCHAR(20),\
                 SID VARCHAR(20),\
                 SURNAME VARCHAR(30),\
@@ -55,7 +53,8 @@ def create_tables(cur):
                 MAJOR VARCHAR(10),\
                 GRADE VARCHAR(10),\
                 STATUS VARCHAR(50),\
-                EMAIL VARCHAR(50)\
+                EMAIL VARCHAR(50),\
+                PRIMARY KEY(CID, TERM, SID)\
     );")
 
 def load():
@@ -70,10 +69,13 @@ def load():
         if(files.endswith(".csv")):
             parse_file(files, cur)
 
+    #cur.execute("COMMIT;")
+
 def insert_into_table(course_tuple, meeting_tuple, seating_tuple,cur): #FIXME: insert multiple tuples at a time rather than one at a time
     ''' inserts a set of tuples into their representive tables'''
 
     CID = course_tuple[0]
+    TERM = course_tuple[1]
     if(CID == ""):
         CID = 'NULL'
     if(len(course_tuple) == 5):
@@ -83,24 +85,24 @@ def insert_into_table(course_tuple, meeting_tuple, seating_tuple,cur): #FIXME: i
         pass
     else:
         cur.execute("INSERT INTO course_tbl( CID,TERM,SUBJ,CRSE,SEC,UNITS)"\
-                     "VALUES (" + CID + ',' + course_tuple[1] + ',\'' + course_tuple[2] + '\',' +  course_tuple[3] + \
+                     "VALUES (" + CID + ',' + TERM + ',\'' + course_tuple[2] + '\',' +  course_tuple[3] + \
                      ',' + course_tuple[4] + ',\''  + course_tuple[5] +"\');")
 
     for tuple in meeting_tuple:
-        cur.execute("INSERT INTO meetings_tbl(CID, INSTRUCTORS,TYPE,DAYS,TIMEE,BUILDING,ROOM)"\
-                     "VALUES (" + CID + ',\'' + tuple[0] + '\',\'' + tuple[1] + '\',\'' +  tuple[2] + \
+        cur.execute("INSERT INTO meetings_tbl(CID, TERM, INSTRUCTORS,TYPE,DAYS,TIMEE,BUILDING,ROOM)"\
+                     "VALUES (" + CID + "," +  TERM +',\'' + tuple[0] + '\',\'' + tuple[1] + '\',\'' +  tuple[2] + \
                      '\',\'' + tuple[3] + '\',\''  + tuple[4] + "\'" + ',\'' + tuple[5]  +"\');")
 
     if(not seating_tuple):
         return
 
     for tuple in seating_tuple:
-        cur.execute("INSERT INTO seating_tbl(CID,SEAT,SID,SURNAME,PREFNAME,LEVEL,UNITS,CLASS,MAJOR,GRADE,STATUS,EMAIL)"\
-                    "VALUES (" + CID + ',\'' + tuple[0] + '\',\'' + tuple[1] + '\',\'' +  tuple[2] + \
+        cur.execute("INSERT INTO seating_tbl(CID, TERM,SEAT,SID,SURNAME,PREFNAME,LEVEL,UNITS,CLASS,MAJOR,GRADE,STATUS,EMAIL)"\
+                    "VALUES (" + CID + "," + TERM + ',\'' + tuple[0] + '\',\'' + tuple[1] + '\',\'' +  tuple[2] + \
                     '\',\'' + tuple[3] + '\',\''  + tuple[4] + "\'" + ',\'' + tuple[5] + '\',\'' \
                      + tuple[6] + '\',\''  + tuple[7] + "\'" + ',\'' + tuple[8] + '\',\'' + tuple[9] + '\',\''  + tuple[10]\
-                     + "\');")
-
+                     + "\') ;")
+#ON DUPLICATE DO NOTHING
 def make_equal(tuple, num_attributes):
 
 
@@ -157,6 +159,7 @@ def parse_meetings(csvreader):
         return meeting_tuple
 
     while(tuple[0] != "" or len(tuple) != 1):
+
         tuple = replace_empty_with_null(tuple)
         if('\'' in tuple[0]):
             tuple[0] = tuple[0].replace('\'','_')
@@ -198,8 +201,8 @@ def parse_seating(csvreader):
 def replace_empty_with_null(tuple):
     ''' finds and replaces all empty attributes with null'''
     refined_tuple = []
-    for index in range(0, len(tuple)): #for some reason the regex split adds a '' to the begining and end of the tuple
-                               # we will account for it here
+    for index in range(0, len(tuple)):
+
         if(tuple[index] == ''):
             refined_tuple.append("NULL")
         else:
