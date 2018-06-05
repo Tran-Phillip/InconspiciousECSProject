af107b259d1ade2f4bb75d14576426eefd0484ce
Phillip N Tran <xizanagi@pc56.cs.ucdavis.edu>
2018-06-04 18:37:52 -0700

    Got first query to work!

--- queryfakeu.py
+++ queryfakeu.py
@@ -18,6 +18,10 @@ def load():
             execute_queries(files, cur)
 
 def execute_queries(files,cur):
+
+    query_3a(cur)
+
+    '''
     query = ""
     with open(files, 'r') as q_file:
         for line in q_file:
@@ -26,6 +30,47 @@ def execute_queries(files,cur):
     query = query.replace('\t','')
     print(query)
     cur.copy_expert(query, sys.stdout)
+
+    '''
+
+def query_3a(cur):
+
+    term = []
+    count_shit = []
+    total_count_shit = []
+
+
+    for i in range(0,20):
+        print("UNITS: ", i)
+        cur.execute("SELECT COUNT(foo.UNITS) , TERM FROM (SELECT UNITS,TERM FROM seating_tbl WHERE UNITS =" + str(i) + ") AS foo\
+          GROUP BY foo.TERM\
+          ORDER BY foo.TERM"
+        )
+
+        x = cur.fetchall()
+
+        for el in x:
+            count_shit.append(el[0])
+            term.append(el[1])
+
+        cur.execute("SELECT COUNT(UNITS), TERM from seating_tbl\
+          GROUP BY TERM\
+          ORDER BY TERM"
+        )
+
+        x2 = cur.fetchall()
+
+        for el in x2:
+            total_count_shit.append(el[0])
+
+        for index in range(0,len(count_shit)):
+            percentage = float(count_shit[index]) / float(total_count_shit[index])
+            print(term[index], percentage)
+
+        term = []
+        count_shit = []
+        total_count_shit = []
+
 
 
 ### Main()
